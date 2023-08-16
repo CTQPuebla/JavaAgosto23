aec478e6c2ee6a4213071d7f504f37a83986cb7c
Selina9824 <zavala.flores2398@gmail.com>
2023-08-16 13:16:11 -0600

    encapsulamiento_szf

---
+++ encapsulamiento_szf/src/com/Principal.java
@@ -0,0 +1,22 @@
+package com;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		Modelo m = new Modelo();
+		
+		//m.x = 20; acceso incorrecto por ser private
+		
+		m.setX(20);//forma correcta de acceder al
+		m.getX(); //atributo private, con los getters & setters
+		
+		m.mensaje="Hola";
+		System.out.println(m.mensaje);
+		m.doSometing();
+		
+		m.decimal = 30.50;
+	}
+
+	
+}
---
+++ encapsulamiento_szf/src/paquete/Externa.java
@@ -0,0 +1,19 @@
+package paquete;
+
+import com.Modelo;
+
+public class Externa extends Modelo {
+	
+	//Modelo model = new Modelo();
+	
+	public Externa() { //constructor vacio
+		
+		//model.mensaje = ""; //acesso incorrecto a un atributo procteted
+		//model.decimal = 93.5;
+		
+		//this.mensaje = "Otra cosa";//unicamente mediante herencia
+		
+	}
+	
+
+}
