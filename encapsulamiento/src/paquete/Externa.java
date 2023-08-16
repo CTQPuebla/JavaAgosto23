9be872e7e02f6de29248feb9d56f2b0063cac99e
CTQPuebla <pruebasceteq@hotmail.com>
2023-08-16 13:13:09 -0600

    encapsulamiento practica

---
+++ encapsulamiento/src/paquete/Externa.java
@@ -0,0 +1,21 @@
+package paquete;
+
+import com.Modelo;
+
+public class Externa extends Modelo{
+
+
+	
+	public Externa() {
+		
+		// model.mensaje = ""; Acceso incorrecto a un atributo protected
+		//model.decimal = 93.5;
+		
+		// this.mensaje = "otra cosa"; Unicamente mediante la herencia es accesible en clases de otros paquetes
+		
+	}
+	
+	
+	
+	
+}
